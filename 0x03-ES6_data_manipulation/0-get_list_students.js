6df12b1de337f5884fc12d0ca9ea2d107b61bd7c
MsOwuor <immaculateadhiambo79@gmail.com>
2024-04-15 10:21:36 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Retrievs a list of students.
+ * @returns {{id: Number, firstname: String, location: String}[]}
+ */
+exportdefault function getListStudents() {
+	return [
+		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
+	];
+}
